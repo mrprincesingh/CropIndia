5d41bcd1ddc078149f4e3a9e54fd29ea8d55445b
mrprincesingh <princeranu98@gmail.com>
2024-03-13 05:01:20 -0400

    Removed the table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import Dashboard from './component/Dashboard/Dashboard';
 import Navbar from './component/Navbar/Navbar';
-import DataTable from './component/table/DataTable';
 
 
 function App() {
